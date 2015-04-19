67c15970eaa5574ab457d5cd5f0962e7e62b9f3f
Sri Mohan <dob.hoy@gmail.com>
2015-04-19 15:43:41 +0100

    Initial Commit

---
+++ Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Srikanth Narayanamohan on 19/04/2015.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

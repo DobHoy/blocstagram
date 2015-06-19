89f91a5fb1dfcbc191adf04d07034c94466ca232
Sri Mohan <dob.hoy@gmail.com>
2015-06-19 17:20:09 +0300

    Before assignement

---
+++ Blocstagram/BLCLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLCLoginViewController.h
+//  Blocstagram
+//
+//  Created by Srikanth Narayanamohan on 16/06/2015.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCLoginViewController : UIViewController
+
+@end

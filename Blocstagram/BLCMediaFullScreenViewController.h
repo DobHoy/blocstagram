630fb4ef87acdde46aa5543b9757cdf240d3ca49
Sri Mohan <dob.hoy@gmail.com>
2015-07-30 17:20:04 +0100

    new cp

---
+++ Blocstagram/BLCMediaFullScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLCMediaFullScreenViewController.h
+//  Blocstagram
+//
+//  Created by Srikanth Narayanamohan on 28/07/2015.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCMediaFullScreenViewController : UIViewController
+
+@end

bfc1759c1e2952dd93eab1a90679084437d27e4f
Sri Mohan <dob.hoy@gmail.com>
2015-07-30 17:20:14 +0100

    new cp2

--- Blocstagram/BLCMediaFullScreenViewController.m
+++ Blocstagram/BLCMediaFullScreenViewController.m
@@ -7,16 +7,43 @@
 //
 
 #import "BLCMediaFullScreenViewController.h"
+#import "BLCMedia.h"
 
-@interface BLCMediaFullScreenViewController ()
+@interface BLCMediaFullScreenViewController () <UIScrollViewDelegate>
+
+@property (nonatomic, strong) BLCMedia *media;
 
 @end
 
 @implementation BLCMediaFullScreenViewController
 
+- (instancetype) initWithMedia:(BLCMedia *) media {
+    self = [super init];
+    
+    if (self) {
+        self.media = media;
+    }
+    
+    
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.scrollView = [UIScrollView new];
+    self.scrollView.delegate = self;
+    self.scrollView.backgroundColor = [UIColor whiteColor];
+    
+    [self.view addSubview:self.scrollView];
+    
+    self.imageView = [UIImageView new];
+    self.imageView.image = self.media.image;
+    
+    [self.scrollView addSubview:self.imageView];
+    self.scrollView.contentSize = self.media.image.size;
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +51,60 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+
+- (void) viewWillLayoutSubviews {
+    
+    [super viewWillLayoutSubviews];
+    self.scrollView.frame = self.view.bounds;
+    
+    CGSize scrollViewFrameSize = self.scrollView.frame.size;
+    CGSize scrollViewContentSize = self.scrollView.contentSize;
+    
+    CGFloat scaleWidth = scrollViewFrameSize.width / scrollViewContentSize.width;
+    CGFloat scaleHeight = scrollViewFrameSize.height / scrollViewContentSize.height;
+    CGFloat minScale = MIN(scaleWidth, scaleHeight);
+    
+    self.scrollView.minimumZoomScale = minScale;
+    self.scrollView.maximumZoomScale = 1;
 }
-*/
+
+- (void)centerScrollView {
+    [self.imageView sizeToFit];
+    
+    CGSize boundsSize = self.scrollView.bounds.size;
+    CGRect contentsFrame = self.imageView.frame;
+    
+    if (contentsFrame.size.width < boundsSize.width) {
+        contentsFrame.origin.x = (boundsSize.width - CGRectGetWidth(contentsFrame)) / 2;
+    } else {
+        contentsFrame.origin.x = 0;
+    }
+    
+    if (contentsFrame.size.height < boundsSize.height) {
+        contentsFrame.origin.y = (boundsSize.height - CGRectGetHeight(contentsFrame)) / 2;
+    } else {
+        contentsFrame.origin.y = 0;
+    }
+    
+    self.imageView.frame = contentsFrame;
+}
+
+#pragma mark - UIScrollViewDelegate
+
+- (UIView*)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return self.imageView;
+}
+
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView {
+    [self centerScrollView];
+}
+
+-(void) viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    [self centerScrollView];
+}
+
 
 @end
